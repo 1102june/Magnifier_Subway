8628885ceb13b3b0cbb27801292d59898ef3161e
SYJ <peter4045123@naver.com>
2026-02-20 15:05:37 +0900

    feat: update User Model to Support Guest Login

---
+++ app/src/main/java/com/example/magnifier_subway/domain/User.kt
@@ -0,0 +1,9 @@
+package com.example.magnifier_subway.domain
+data class User(
+    val uid: String,
+    //Guest든 Google이든 고유식별자가 존재
+    val email: String? = null,
+    // Guest는 이메일이 없으므로 Null
+    val isGuest: Boolean = false
+    // 해당 유저가 게스트인지 아닌지 확인
+)
