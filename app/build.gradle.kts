8a0ce5d71e4518be1014e29b4bbdcbb46a21357d
SYJ <peter4045123@naver.com>
2026-02-27 16:02:24 +0900

    chore: add Credential Libraries

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,4 +73,10 @@ dependencies {
 
     // Hilt Navigation Compose
     implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
+
+    //Credential Manager
+    implementation("androidx.credentials:credentials:1.2.2")
+    implementation("androidx.credentials:credentials-play-services-auth:1.2.2")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
+
 }
