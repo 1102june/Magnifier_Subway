7c1c5dd0996978fb299f3ba055126887e29cf5f1
SYJ <peter4045123@naver.com>
2026-03-04 18:10:09 +0900

    Station Entity

--- app/src/main/java/com/example/magnifier_subway/data/entity/StationEntity.kt
+++ app/src/main/java/com/example/magnifier_subway/data/entity/StationEntity.kt
@@ -1,4 +1,18 @@
 package com.example.magnifier_subway.data.entity
+/*DB -> 역이름에 인덱스 걸기*/
+
+import androidx.room.Entity
+import androidx.room.Index
+import androidx.room.PrimaryKey
 
 class StationEntity {
+    @Entity(tableName = "subway_stations",
+        indices = [Index(value = ["stationName"])]
+    )
+    data class  StationEntity(
+        @PrimaryKey val stationId: String,  //공공데이터 역 고유코드
+        val Stationname: String,  //역이름
+        val lineNumber: String,   //호선
+        val isFavorite: Boolean = false // 즐겨찾기 여부
+    )
 }
