0ab24038f618f706ee3121826cdad18764cbab63
SYJ <peter4045123@naver.com>
2026-03-04 18:18:50 +0900

    Updating Station Dao Query

--- app/src/main/java/com/example/magnifier_subway/data/dao/StationDao.kt
+++ app/src/main/java/com/example/magnifier_subway/data/dao/StationDao.kt
@@ -1,4 +1,28 @@
 package com.example.magnifier_subway.data.dao
 
-class StationDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.magnifier_subway.data.entity.StationEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface StationDao{
+    @Query("SELECT * FROM subway_stations WHERE stationName LIKE '%' || :query || '%'")
+    fun searchStations(query: String): Flow<List<StationEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(stations: List<StationEntity>)
+    // 초기 데이터 저장 << 공공데이터 대량 저장
+
+
+    @Query("SELECT * FROM subway_stations WHERE stationId = :id")
+    suspend fun getStationById(id:String): StationEntity?
+    // 역정보 가져오기
+
+    @Update
+    suspend fun updateStation(station: StationEntity)
+    //즐겨찾기 상태변경
 }
